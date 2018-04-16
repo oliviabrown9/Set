ec00083a76c33e3cf7744362ac419da186d10afb
Olivia <obrown917@gmail.com>
2018-04-15 20:50:44 -0700

    Card struct

---
+++ Set/Card.swift
@@ -0,0 +1,35 @@
+//
+//  Card.swift
+//  Set
+//
+//  Created by Olivia Brown on 4/15/18.
+//  Copyright © 2018 Olivia Brown. All rights reserved.
+//
+
+import Foundation
+
+struct Card: Hashable {
+    
+    var hashValue: Int {
+        return identifier
+    }
+    
+    static func ==(lhs: Card, rhs: Card) -> Bool {
+        return lhs.identifier == rhs.identifier
+    }
+    
+    var isFaceUp = false
+    var isInSet = false
+    private var identifier: Int
+    
+    private static var identifierFactory = 0
+    
+    private static func getUniqueIdentifier() -> Int {
+        identifierFactory += 1
+        return identifierFactory
+    }
+    
+    init() {
+        self.identifier = Card.getUniqueIdentifier()
+    }
+}
---
+++ Set/Set.swift
@@ -0,0 +1,9 @@
+//
+//  Set.swift
+//  Set
+//
+//  Created by Olivia Brown on 4/15/18.
+//  Copyright © 2018 Olivia Brown. All rights reserved.
+//
+
+import Foundation
